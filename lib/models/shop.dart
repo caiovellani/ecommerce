37c0c454c790323bd10c08d7ced92b0156e0861a
caiovellani <caiovellani@outlook.com>
2024-07-25 22:40:27 -0300

    feat: "create product and shop models"

---
+++ lib/models/shop.dart
@@ -0,0 +1,46 @@
+import 'product.dart';
+
+class Shop {
+  // list of products for sale
+  final List<Product> _shop = [
+    Product(
+      name: 'Product 1',
+      price: 99.99,
+      description: 'Item description...',
+    ),
+    Product(
+      name: 'Product 2',
+      price: 99.99,
+      description: 'Item description...',
+    ),
+    Product(
+      name: 'Product 3',
+      price: 99.99,
+      description: 'Item description...',
+    ),
+    Product(
+      name: 'Product 4',
+      price: 99.99,
+      description: 'Item description...',
+    ),
+  ];
+
+  // user cart
+  final List<Product> _cart = [];
+
+  // get product list
+  List<Product> get shop => _shop;
+
+  // get user cart
+  List<Product> get cart => _cart;
+
+  // add item to cart
+  void addToCart(Product item) {
+    _cart.add(item);
+  }
+
+  // remove item from cart
+  void removeFromCart(Product item) {
+    _cart.remove(item);
+  }
+}
