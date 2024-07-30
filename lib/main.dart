10613a4ee0b089488005f9638c83368287eec387
caiovellani <caiovellani@outlook.com>
2024-07-29 22:14:54 -0300

    feat: add provider to main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'models/shop.dart';
 import 'pages/cart_page.dart';
 import 'pages/intro_page.dart';
 import 'pages/shop_page.dart';
 import 'themes/light_mode.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => Shop(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,13 +22,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: lightMode,
-        debugShowCheckedModeBanner: false,
-        home: const IntroPage(),
-        routes: {
-          '/intro': (context) => const IntroPage(),
-          '/shop': (context) => const ShopPage(),
-          '/cart': (context) => const CartPage(),
-        });
+      theme: lightMode,
+      debugShowCheckedModeBanner: false,
+      home: const IntroPage(),
+      routes: {
+        '/intro': (context) => const IntroPage(),
+        '/shop': (context) => const ShopPage(),
+        '/cart': (context) => const CartPage(),
+      },
+    );
   }
 }
